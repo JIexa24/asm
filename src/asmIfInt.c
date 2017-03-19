87e7156482f885b29e47c0a0fdf5d33d23eadd69
Alexey R <lexa260011@ngs.ru>
2017-03-19 07:17:46 +0000

    asmIfInt (ret -1|0|1)

---
+++ src/asmIfInt.c
@@ -0,0 +1,22 @@
+
+int asmIfInt(int a, int b) {
+  int ret = -2;
+
+  asm volatile (".intel_syntax noprefix\n\t"
+                "cmp %1, %2\n\t"
+                "jg big\n\t"
+                "jl low\n\t"
+                "mov %0, 0\n\t"
+                "jmp end \n"
+                "big:\n\t"
+                "mov %0, 1\n\t"
+                "jmp end \n"
+                "low:\n\t"
+                "mov %0, -1\n"
+                "end:\n"
+                : "=r"(ret)
+                : "r" (a), "r"(b)
+                : "%eax"
+               );
+  return ret;
+}
