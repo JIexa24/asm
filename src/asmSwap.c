5308f41a12e250146205d5b3ae5acfdd879e4cc9
Alexey R <lexa260011@ngs.ru>
2017-03-19 06:14:39 +0000

    asmSwap

---
+++ src/asmSwap.c
@@ -0,0 +1,11 @@
+
+void asmSwap(int* a, int* b) {
+  asm volatile (".intel_syntax noprefix\n\t"
+                "mov eax, %2\n\t"
+                "mov %0, %3\n\t"
+                "mov %1, eax\n\t"
+                : "=r" (*a), "=r"(*b)	
+                : "r" (*a), "r"(*b)
+                : "%eax"
+               );
+}
